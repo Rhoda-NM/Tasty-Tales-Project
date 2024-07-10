23c44b936f35c377f68af340ec6b6d281e5a94d2
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-07-10 13:56:04 +0300

    created models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model):
+    pass
+class Recipes(db.Model):
+    pass
+class Reviews(db.Model):
+    pass
+class Users(db.Model):
+    pass
+class Tags(db.Model):
+    pass

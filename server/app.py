bbc8ead48420a7cc978250f548edf4bd4cac7282
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-07-11 22:33:03 +0300

    set up and initialize the app.py

--- server/app.py
+++ server/app.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request, make_response, jsonify, session
+from flask_restful import Resource, Api  #  imported Api
+from flask_migrate import Migrate  #  imported Migrate
 
 # Local imports
-from config import app, db, api
+from config import app, db 
 # Add your model imports
 from models import User, Recipes, Rating, RecipeTag, Reviews, Tags
 
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+# Initialize database and migration
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# Initialize API
+api = Api(app)
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+class Login(Resource):
+
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 200
+
 
 
 if __name__ == '__main__':

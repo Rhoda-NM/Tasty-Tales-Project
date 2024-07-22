e1d472ea839195e589bfa22fc2067a5481940227
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-07-22 07:32:21 +0300

    Enance footer styling

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import './footer.css';
 
 const Footer = () => {
+  const footerStyle = {
+    backgroundImage: 'url("https://images.pexels.com/photos/2156881/pexels-photo-2156881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    color: 'white',
+    padding: '20px'
+  };
+
+  const footerContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between'
+  };
+
+  const footerBottomStyle = {
+    textAlign: 'center',
+    padding: '10px',
+    background: 'rgba(0, 0, 0, 0.5)',
+    color: 'white'
+  };
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none'
+  };
+
+  const linkHoverStyle = {
+    textDecoration: 'underline'
+  };
+
   return (
-    <footer className="footer" style={{background: "#eeeeea"}}>
-      <div className="footer-container">
+    <footer className="footer" style={footerStyle}>
+      <div className="footer-container" style={footerContainerStyle}>
         <div className="footer-column">
           <h4>About Us</h4>
           <ul>
@@ -17,17 +46,17 @@ const Footer = () => {
         <div className="footer-column">
           <h4>Support</h4>
           <ul>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Advertise</a></li>
+            <li><a href="#" style={linkStyle}>Contact us</a></li>
+            <li><a href="#" style={linkStyle}>FAQs</a></li>
+            <li><a href="#" style={linkStyle}>Advertise</a></li>
           </ul>
         </div>
         <div className="footer-column">
           <h4>Follow us</h4>
           <ul className="social-links">
-            <li><a href="#"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
+            <li><a href="#" style={linkStyle}><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
+            <li><a href="#" style={linkStyle}><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
+            <li><a href="#" style={linkStyle}><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
           </ul>
         </div>
         <div className="footer-column">
@@ -39,7 +68,7 @@ const Footer = () => {
           </form>
         </div>
       </div>
-      <div className="footer-bottom">
+      <div className="footer-bottom" style={footerBottomStyle}>
         <p>&copy; 2024 Tasty Tales Recipes. All rights reserved.</p>
       </div>
     </footer>
